64b9e7639511a0060b012ca796a72b7d44bbdccc
Anchit Gupta <anchitgupt2012@gail.com>
2019-02-21 14:07:00 +0530

    bug fix
    bug fix

--- server.js
+++ server.js
@@ -5,23 +5,35 @@ const apiKey = 'acf290298ef501c8ea98427a8226314c';
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended:true}));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+  console.log(url);
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render('index', {
+        weather: null,
+        error: 'Error, please try again'
+      });
     } else {
       let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+      if (weather.main == undefined) {
+        res.render('index', {
+          weather: null,
+          error: 'Error, please try again'
+        });
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        res.render('index', {
+          weather: weatherText,
+          error: null
+        });
       }
     }
   });
